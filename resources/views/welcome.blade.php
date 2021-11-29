596c6f17d091ef40ce1e11ea8983d8aeae9a16f9
najeebamtk <najeebamtk1995@gmail.com>
2021-11-29 12:01:21 +0530

    welcome page modified

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body bgcolor="yellow">
     <h1>PROJECT</h1>
     <a href="{{url('/')}}">HOME</a>&nbsp;&nbsp;
 
