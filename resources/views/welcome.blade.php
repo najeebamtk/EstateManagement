a52389c2ce4de8364d758f69d348806eb7cc909d
najeebamtk <najeebamtk1995@gmail.com>
2021-11-29 11:37:35 +0530

    heroku

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PROJECT</h1>
+    <a href="{{url('/')}}">HOME</a>&nbsp;&nbsp;
+
+    <a href="{{url('about')}}">ABOUT</a>&nbsp;&nbsp;
+    <a href="{{url('contact')}}">CONTACT</a>&nbsp;&nbsp;
+    <a href="{{url('form')}}">FORM</a>&nbsp;&nbsp;<br>
+    <img src="{{asset('images/logo.png')}}">
+    <img src="{{asset('images/1.jpg')}}" width="400" height="400">
+
+</body>
+</html>
